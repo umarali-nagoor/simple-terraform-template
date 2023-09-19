6fd8d7bc54e1f4f5636d1b7fb10164f00a7ebd2f
Umar Ali <umarali@Umars-MacBook-Pro.local>
2023-09-19 11:59:58 +0530

    Added default values

--- vars.tf
+++ vars.tf
@@ -2,13 +2,16 @@
 
 variable "TYPE" {
   description = "The name to pass to the template."
+  default = "tyre1"
 }
 
 variable "location" {
   description = "Location to deploy"
+  default = "us-south"
 }
 
 variable "flavor" {
   description = "Falvor type."
+  default = "c3f6"
 }
 

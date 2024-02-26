85c3f369b7bd71c2dcf2d7a52d1a3fbf217e2fa1
Umar Ali <umarali@Umars-MacBook-Pro.local>
2024-02-26 16:28:48 +0530

    Initial version

--- output.tf
+++ output.tf
@@ -2,10 +2,10 @@ output "template_file" {
     value = data.template_file.test.rendered
 }
 
-output "template_file_new" {
+/*output "template_file_new" {
     value = data.template_file.test2.rendered
 }
 
 output "template_file_new2" {
     value = data.template_file.test3.rendered
-}
+}*/
